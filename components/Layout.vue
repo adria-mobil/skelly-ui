d08d1ce8ed06fe05de5e5c051cc19c19dbd620f2
slovenianGooner <lovro.papez@gmail.com>
2022-11-28 11:08:55 +0100

    Fix logo height and menu overflow

--- components/Layout.vue
+++ components/Layout.vue
@@ -91,14 +91,15 @@ defineExpose({
         </div>
         <div class="drawer-side overflow-x-hidden">
             <label for="sidebar-drawer" class="drawer-overlay"></label>
-            <aside class="bg-base-300 w-80">
+            <aside class="bg-base-300 w-80 overflow-hidden flex flex-col">
                 <div
-                    class="z-20 bg-accent backdrop-blur sticky top-0 items-center gap-2 px-4 py-2 hidden shadow-sm"
+                    class="z-20 h-16 flex bg-accent backdrop-blur items-center gap-2 px-4 py-2 hidden shadow-sm"
                     :class="[desktopBreakpoint]">
                     <slot name="logo"/>
                 </div>
-                <div class="h-4"></div>
-                <slot name="navigation"/>
+                <div class="flex-1 overflow-y-auto">
+                    <slot name="navigation"/>
+                </div>
             </aside>
         </div>
     </div>
