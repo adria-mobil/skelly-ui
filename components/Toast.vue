9e838b0ebd59de649147914069db7e11b56e3b2d
Blaz Gregorcic <gregorcic.blaz@gmail.com>
2022-12-05 10:34:53 +0100

    Toast timeout.

--- components/Toast.vue
+++ components/Toast.vue
@@ -1,15 +1,33 @@
 <script setup>
-defineProps({
+import {onMounted} from "vue";
+
+const emit = defineEmits(['hide'])
+
+const props = defineProps({
     alertClass: {
         type: String,
         default: "alert-warning"
+    },
+    timeout: {
+        type: Number,
+        default: 0,
+        required: false,
     }
 })
+
+onMounted(() => {
+    if (props.timeout > 0) {
+        setTimeout(function () {
+            emit('hide');
+        }, props.timeout);
+    }
+})
+
 </script>
 <template>
     <div class="toast">
         <div class="alert" :class="[alertClass]">
-            <slot />
+            <slot/>
         </div>
     </div>
 </template>
